c45ef6876e7c98f1a0193ef1f26d9a3b94e9d519
Philip Pfarrhofer <p.pfarrhofer@students.htl-leonding.ac.at>
2024-01-18 11:11:37 +0100

    php added and backend js updated

---
+++ php/server.php
@@ -0,0 +1,61 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+use Ratchet\Http\HttpServer;
+
+class ChatServer implements MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+
+        $this->clients = new \SplObjectStorage;
+        echo "Server started on Port 8080! \n";
+        echo "Press Ctr+C to Quit \n";
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send("Ready status set to: " + $msg);
+            }
+        }
+
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ChatServer()
+        )
+    ),
+    8080
+);
+
+$server->run();
---
+++ script/index_b.js
@@ -0,0 +1,18 @@
+/* CHANGE THIS TO YOUR SERVERS IP-ADRESS =>*/ const address = "localhost"; const Protocol = "ws";
+
+
+console.log(`${Protocol}://${address}`)
+const socket = new WebSocket(`${Protocol}://${address}`);
+
+socket.onopen = function (event) {
+    console.log('WebSocket is connected.');
+    sendServerMessage(`${localStorage.getItem("username")} has joined the Channel.`);
+};
+
+socket.onmessage = function (event) {
+    console.log(event);
+};
+
+function setReadyStatus(ready_status) {
+    socket.send(ready_status);
+}
