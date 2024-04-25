57b6fd71ad5aca81e019aa6aeb9c866020dd9407
RaphRei <reisinger.raphael12@gmail.com>
2024-04-25 10:43:24 +0200

    kategorien

--- Script/Json/categories.js
+++ Script/Json/categories.js
@@ -1,8 +1,72 @@
 let categories = {
     "Standard": [
-        { "category1": "" },
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "Gewässer" },
+        { "category1": "Name" },
+        { "category1": "Tier" },
+        { "category1": "Beruf" },
+        { "category1": "Pflanze" },
+        { "category1": "Marke" }
+    ],
+    "Sport": [
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "Sportart" },
+        { "category1": "Sportmarke" },
+        { "category1": "Vereinsname" },
+        { "category1": "SportlerIn" },
+        { "category1": "TrainerIn" },
+        { "category1": "Sportutensil" }
+    ],
+    "Rotlicht": [
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "nimmt man in den Mund" },
+        { "category1": "Pornokategorie" },
+        { "category1": "Pornodarsteller" },
+        { "category1": "Sexstellung" },
+        { "category1": "Sexspielzeug" },
+        { "category1": "ruft man beim Sex" }
     ],
     "Lask": [
-        { "name": "Lucky" },
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "Ehem. Spieler" },
+        { "category1": "Akt. Spieler" },
+        { "category1": "Trainer" },
+        { "category1": "Verein 1.&2. Bundesliga" },
+        { "category1": "Grund für Stadionverbot" },
+        { "category1": "Dressfarbe" }
+    ],
+    "Rennsport": [
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "Streckenname" },
+        { "category1": "Autohersteller" },
+        { "category1": "Rennsport-Team" },
+        { "category1": "Rennfahrer" },
+        { "category1": "Autoteil" },
+        { "category1": "Rennserie" }
+    ],
+    "Haushalt": [
+        { "category1": "Stadt" },
+        { "category1": "Land" },
+        { "category1": "Haushaltgerät" },
+        { "category1": "Reinigungsmittel" },
+        { "category1": "Möbelstück" },
+        { "category1": "Lebensmittel" },
+        { "category1": "Pflanze im Haushalt" },
+        { "category1": "Gartengerät" }
+    ],
+    "Tierliebhaber": [
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" },
+        { "category1": "" }
     ]
 }
